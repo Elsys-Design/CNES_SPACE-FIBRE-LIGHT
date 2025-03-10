303f80fe13e1698c3c0c7c5733fbff39de8ac4d8
uf258tfa <thomas.favre-felix@elsys-design.com>
2025-03-10 17:46:27 +0100

    add progress on archived repository to the clean repository, fix loopback script, adapt runsim.sh script to custom setup

--- IP_SPACE_FIBRE/02_dev/03_verification/05_scenario/RunSim.sh
+++ IP_SPACE_FIBRE/02_dev/03_verification/05_scenario/RunSim.sh
@@ -10,21 +10,21 @@
 #set your variable below
 
 #set you absolute path to the root of the project spacefibrelight
-export SPACEFIBRELIGHT_ROOT_PATH=/home/flo/FLORENT/GIT/Spacefibrelight/24-9771-ed_cnes_ip-space-fiber-CNES-rev
+export SPACEFIBRELIGHT_ROOT_PATH=/mnt/6ae9ae0d-927d-480b-9d98-fd506029b645/CNES_Project/24-9771-ED_CNES_IP-SPACE-FIBRE
 
 #set your framework location
-export FRAMEWORK_COCOTB_INSTALL_PATH=/home/flo/FLORENT/GIT/24-9771-ed-cnes_test-cocotb-framework
+export FRAMEWORK_COCOTB_INSTALL_PATH=/mnt/6ae9ae0d-927d-480b-9d98-fd506029b645/CNES_Project/cosim_environment
 
 # Select 1 to have to gui loaded (you will have to click run all to start simualtion)
 # select 0 to have automatic
-export GUI=0
+export GUI=1
 
 # Select 1 to create the full waveform for the test (questa WLF file)
 # select 0 not ot generate the waveforms
-export WAVES=0
+export WAVES=1
 
 ##additionnal VSIM command  
-export EXTRA_VSIM_CMD="-do $SPACEFIBRELIGHT_ROOT_PATH/IP_SPACE_FIBRE/02_dev/03_verification/05_scenario/custom.do"
+export EXTRA_VSIM_CMD="-do $SPACEFIBRELIGHT_ROOT_PATH/IP_SPACE_FIBRE/02_dev/03_verification/05_scenario/fast_sim.do"
 
 #############################################
 
@@ -56,7 +56,9 @@ export PYTHONPATH=$current_dir/../02_benches/configuration_1_bench:$PYTHONPATH
 export PYTHONPATH=$current_dir/../02_benches/common:$PYTHONPATH
 export PYTHONPATH=$current_dir/../01_models/python_model:$PYTHONPATH
 #add framework to pythonpath
-export PYTHONPATH=$FRAMEWORK_COCOTB_INSTALL_PATH/src:$PYTHONPATH
+source $FRAMEWORK_COCOTB_INSTALL_PATH/cosim_env/bin/activate
+export PYTHONPATH=$FRAMEWORK_COCOTB_INSTALL_PATH:$PYTHONPATH
+export PYTHONPATH=$FRAMEWORK_COCOTB_INSTALL_PATH/23-9950-ED_INTERNE_DIGITAL-VERIFICATION-PYTHON/src:$PYTHONPATH
 #display python path environment for debug purpose
 #echo $PYTHONPATH
 
---
+++ IP_SPACE_FIBRE/02_dev/03_verification/05_scenario/fast_sim.do
@@ -0,0 +1,3 @@
+profile on
+profile off
+profile clear
